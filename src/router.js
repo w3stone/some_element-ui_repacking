884e6254b1c0bf7a7d34f726c409724ee78eb9f6
wjl <wjl.1993@gmail.com>
2018-12-09 21:04:38 +0800

    20181209

--- src/router.js
+++ src/router.js
@@ -6,7 +6,7 @@ import homeIndex from '@/views/index'
 Vue.use(Router);
 
 const router = new Router({
-    mode: 'history',
+    //mode: 'history',
     routes: [
         {
             path: '/index',
